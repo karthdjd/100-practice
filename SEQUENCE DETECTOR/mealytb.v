9eab7c9b3b542955f28b248b69dfb97644ac45e9
Karthikeyan S <103479792+karthdjd@users.noreply.github.com>
2023-02-06 08:51:08 +0530

    Add files via upload

---
+++ SEQUENCE DETECTOR/mealytb.v
@@ -0,0 +1,35 @@
+module mealytb;
+  reg clk, rst_n, x;
+  wire z,state,next_state;
+  
+ mealy sd(clk, rst_n, x, z);
+  initial clk = 0;   
+  always #2 clk = ~clk;
+    
+  initial begin
+    $monitor("%0t: x = %0b, z = %0b", $time, x, z);
+    x = 0;
+    #1 rst_n = 0;
+    #2 rst_n = 1;
+    
+    #3 x = 1;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #10;
+    $finish;
+  end
+  
+  initial begin
+    
+    $dumpfile("mealy.vcd");
+    $dumpvars(0);
+  end
+endmodule
