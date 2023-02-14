096c09283dec24d06fe3745bd5fb41345d47ec19
Karthikeyan S <103479792+karthdjd@users.noreply.github.com>
2023-02-14 22:20:08 +0530

    Create odd_parity_tb.v

---
+++ PARITY GEN/odd_parity_tb.v
@@ -0,0 +1,20 @@
+module odd_parity_tb.v;
+  reg a,b,c,P;
+  wire PEC;
+  odd_parity dut(a,b,c,P,out);
+  initial begin
+  
+    $monitor($time,"value of a=%b,b=%b,c=%b,P=%b,PEC=%b",a,b,c,P,PEC);
+    $dumpfile("odd_parity.vcd");
+    $dumpvars(1);
+    a=0;b=0;c=0;P=0;
+  end
+  always #80 a=~a;
+  always #40 b=~b;
+  always #20 c=~c;
+  always #10 P=~P;
+  always@(a,b,c,P)
+    begin
+      #200 $finish;
+    end
+endmodule
